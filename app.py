78d018e03a6100d0194bc7f18049c5008075bc72
Kuntala Sri <srikuntala9@gmail.com>
2026-09-18 12:14:56 +0000

    Add CORS for classification frontend

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import numpy as np
@@ -34,7 +34,13 @@ app = FastAPI(
     description="FastAPI for AQI Category Prediction",
     version="1.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://aqi-classification-frontend.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class AQIInput(BaseModel):
     hour_00: float
